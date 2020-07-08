69a57c5e057cc889bd4d85f01f7550003f21c311
vishal <vishal@ascenwork.com>
2020-07-08 18:20:24 +0530

    first

---
+++ APAR_ExpenseVoucher_Mail_Schedular/APAR_ExpenseVoucher_Mail_Schedular/Models/CommonClasses.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APAR_ExpenseVoucher_Mail_Schedular.Models
+{
+
+
+    public class ExpenseVoucher
+    {
+        public int Id { get; set; }
+        public string ExpVoucherNo { get; set; }
+        public string CreatorName { get; set; }
+        public string CreatorDepartment { get; set; }
+        public string CreatorLocation { get; set; }
+        public string Destination { get; set; }
+        public string VisitDate { get; set; }
+        public string StatusCode { get; set; }
+        public string StatusName { get; set; }
+        public string AssignUser { get; set; }
+        public string MultipleApprover { get; set; }
+        public string CreationDate { get; set; }
+        public string FunctionalHead { get; set; }
+        public string SequenceNo { get; set; }
+        public string AssignDate { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeNumber { get; set; }
+        public string Designation { get; set; }
+        public string ActionTaken { get; set; }
+        public string CompanyCode { get; set; }
+        public string DivisionName { get; set; }
+        public string ActiveYear { get; set; }
+        public string Modified { get; set; }
+        //public string TravelType { get; set; }
+        public string CurrentApprove { get; set; }
+        public string AccountApprover { get; set; }
+        public string AccountUserApproved { get; set; }
+
+    }
+
+
+
+}
---
+++ APAR_ExpenseVoucher_Mail_Schedular/APAR_ExpenseVoucher_Mail_Schedular/Program.cs
@@ -0,0 +1,74 @@
+﻿using APAR_ExpenseVoucher_Mail_Schedular.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using APAR_EmployeeMaster_Schedular.Models;
+
+namespace APAR_ExpenseVoucher_Mail_Schedular
+{
+    class Program
+    {
+        static void Main()
+        {
+            //string filename = "log\\Log.txt";
+            //CustomSharePointUtility.logFile = new StreamWriter(filename);
+            //CustomSharePointUtility.WriteLog("*********************************************");
+            //CustomSharePointUtility.WriteLog("Reminder Mail Starts: " + DateTime.Now.ToString());
+            //CustomSharePointUtility.WriteLog("*********************************************");
+            //Console.WriteLine("*********************************************");
+            //Console.WriteLine("Reminder Mail starts : " + DateTime.Now.ToString());
+            //Console.WriteLine("*********************************************");
+            List<ExpenseVoucher> SPTravelVoucher = null;
+            try
+            {
+                var siteUrl = ConfigurationManager.AppSettings["SP_Address_Live"];
+                string TestingExpenseHeaderList = ConfigurationManager.AppSettings["TestingExpenseHeaderList"];
+                string EmailList = ConfigurationManager.AppSettings["EmailList"];
+                string DaysDifference = ConfigurationManager.AppSettings["DaysDifference"];
+                //string query = SQLUtility.ReadQuery("EmployeeMasterQuery.txt");
+                SPTravelVoucher = new List<ExpenseVoucher>();
+                //Task task_SPEmployeeMaster = Task.Run(() => SPTravelVoucher = CustomSharePointUtility.GetAll_TravelVoucherFromSharePoint(siteUrl, TestingTravelHeaderList));
+                SPTravelVoucher = CustomSharePointUtility.GetAll_TravelVoucherFromSharePoint(siteUrl, TestingExpenseHeaderList, DaysDifference);
+                //List<TravelVoucher> empMasterFinal = new List<TravelVoucher>();
+                List<ExpenseVoucher> empMasterFinal = SPTravelVoucher;
+                if (empMasterFinal.Count > 0)
+                {
+                    //CustomSharePointUtility.WriteLog("Voucher data successfully.");
+                    //Console.WriteLine("Employee data synchronized successfully.");
+                    var success = CustomSharePointUtility.EmailData(empMasterFinal, siteUrl, EmailList);
+                    if (success)
+                    {
+                        //CustomSharePointUtility.WriteLog("Reminder Mail Sent Successfully.");
+                        //Console.WriteLine("Reminder Mail Sent Successfully.");
+                    }
+                }
+                else
+                {
+                    //CustomSharePointUtility.WriteLog("No employee data to synchronize.");
+                    //Console.WriteLine("No employee data to synchronize.");
+                }
+            }
+            catch (Exception ex)
+            {
+                //CustomSharePointUtility.WriteLog("Error in scheduler : " + ex.StackTrace);
+                //Console.WriteLine("Error in scheduler : " + ex.StackTrace);
+            }
+            finally
+            {
+                //CustomSharePointUtility.WriteLog("*********************************************");
+                //CustomSharePointUtility.WriteLog("Reminder Mail ends : " + DateTime.Now.ToString());
+                //CustomSharePointUtility.WriteLog("*********************************************");
+                //Console.WriteLine("*********************************************");
+                //Console.WriteLine("Reminder Mail ends : " + DateTime.Now.ToString());
+                //Console.WriteLine("*********************************************");
+             //   CustomSharePointUtility.logFile.Close();
+                //Console.ReadKey();
+
+            }
+        }
+    }
+}
